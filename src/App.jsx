9b3e79821556f5ce63ffc02e8802feca4f31a64f
Andrea <andrearivas4cinfor@gmail.com>
2023-04-13 22:15:59 +0200

    Add navigation skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,38 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
-      <h1>This is our main component</h1>
-    </div>
+     const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    AOS.init();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return (
+    <>
+      <HashRouter>
+        {width < 769 ? <NavigationMobile /> : <Navigation />}
+        <hr id="top" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {width < 768 ? (
+            <Route path="/about" element={<AboutPhone />} />
+          ) : (
+            <Route path="/about" element={<About />} />
+          )}
+          
+          <Route path="/bookings" element={<Bookings />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/sophrology" element={<Sophrology />} />
+          <Route path="/massages" element={<Massages />} />
+          <Route path="*" element={<Default404 />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
+    </>
+  )
   )
 }
 
