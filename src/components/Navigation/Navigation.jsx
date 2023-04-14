0a32c57cc239316df65d4e10bd67adefdf29e6c4
Andrea <andrearivas4cinfor@gmail.com>
2023-04-14 15:48:11 +0200

    improve style

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,43 +10,49 @@ return (
     <Link to="/" className="logo-container">
       <img className="logo-icon" src={logoIcon}></img>
     </Link>
-    <div className="vertical-line"></div>
-    <select name="genres" className="genre-selections">
-      <option value="all">All genres</option>
-      <option value="fighting">Fighting</option>
-      <option value="shooter">Shooter</option>
-      <option value="music">Music</option>
-      <option value="platform">Platform</option>
-      <option value="puzzle">Puzzle</option>
-      <option value="racing">Racing</option>
-      <option value="real-time-strategy-rts">Real Time Strategy (RTS)</option>
-      <option value="role-playing-rpg">Role-playing (RPG)</option>
-      <option value="simulator">Simulator</option>
-      <option value="sport">Sport</option>
-    </select>
-    <div className="vertical-line"></div>
-    <form className="search-form">
-      <input
-        className="search-input"
-        type="text"
-        placeholder="SEARCH A GAME.."
-      ></input>
-      <button className="search-btn">
-        <BsSearch />
-      </button>
-    </form>
+    <div className="filter-container">
+      <div className="vertical-line"></div>
+      <select name="genres" className="genre-selections">
+        <option value="all">All genres</option>
+        <option value="fighting">Fighting</option>
+        <option value="shooter">Shooter</option>
+        <option value="music">Music</option>
+        <option value="platform">Platform</option>
+        <option value="puzzle">Puzzle</option>
+        <option value="racing">Racing</option>
+        <option value="real-time-strategy-rts">Real Time Strategy (RTS)</option>
+        <option value="role-playing-rpg">Role-playing (RPG)</option>
+        <option value="simulator">Simulator</option>
+        <option value="sport">Sport</option>
+      </select>
+      <div className="vertical-line"></div>
+    </div>
+    <div className="form-container">
+      <div className="vertical-line"></div>
+      <form className="search-form">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="SEARCH A GAME..."
+        ></input>
+        <button className="search-btn">
+          <BsSearch />
+        </button>
+      </form>
+      <div className="vertical-line"></div>
+    </div>
     <div className="icons-container">
       <div className="vertical-line"></div>
       <Link to="/" className="my-profile link-icon">
-        <AiFillHeart />
+        <AiFillHeart className="nav-logo" />
       </Link>
       <div className="vertical-line"></div>
       <Link to="/my-profile" className="my-cart link-icon">
-        <BsFillCartFill />
+        <BsFillCartFill className="nav-logo" />
       </Link>
       <div className="vertical-line"></div>
       <Link to="/cart" className="my-cart link-icon">
-        <BsFillPersonFill />
+        <BsFillPersonFill className="nav-logo" />
       </Link>
       <div className="vertical-line"></div>
     </div>
