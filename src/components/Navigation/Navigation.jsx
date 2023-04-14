7c830d7f78e4af84e3276c553b6af282a51f117a
Andrea <andrearivas4cinfor@gmail.com>
2023-04-14 12:57:10 +0200

    finish style navbar for desktop

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,20 +10,23 @@ return (
     <Link to="/" className="logo-container">
       <img className="logo-icon" src={logoIcon}></img>
     </Link>
+    <div className="vertical-line"></div>
     <select name="genres" className="genre-selections">
       <option>All genres</option>
     </select>
+    <div className="vertical-line"></div>
     <form className="search-form">
       <input
         className="search-input"
         type="text"
-        placeholder="Search a game"
+        placeholder="SEARCH A GAME.."
       ></input>
       <button className="search-btn">
         <BsSearch />
       </button>
     </form>
     <div className="icons-container">
+      <div className="vertical-line"></div>
       <Link to="/" className="my-profile link-icon">
         <AiFillHeart />
       </Link>
@@ -33,7 +36,6 @@ return (
       </Link>
       <div className="vertical-line"></div>
       <Link to="/" className="my-cart link-icon">
-        
         <BsFillPersonFill />
       </Link>
       <div className="vertical-line"></div>
