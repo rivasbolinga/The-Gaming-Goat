1b95f1f6b3f0e9b47640ced1ad77a16b392107b7
thugperfect <meenamani9444@gmail.com>
2023-07-26 17:40:43 +0000

    RAWG API...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const [gameName ,setGameName] = useState("")
 
   const API_key = "3ba5d72b5215437dbb25e3ab5bc18d46"
 useEffect(()=>{
-axios.get(`https://api.rawg.io/api/games?key=${API_key}&dates=2019-09-01,2019-09-30&platforms=18,1,7`)
+axios.get(`https://api.rawg.io/api/games?key=${API_key}&platforms=18,1,7`)
 .then((res)=>{
   setGameName(res.data)
   console.log(res.data);
