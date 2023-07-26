6d05f76b20a0e07a3ccb783922d18c26224cfc02
thugperfect <meenamani9444@gmail.com>
2023-07-26 17:38:23 +0000

    RAWG API...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import GameBox from '../components/GameBox'
 import './pages.css'
 import '../../games.json'
+import axios from 'axios';
+import { useState,useEffect } from 'react';
 function Home() {
+const [gameName ,setGameName] = useState("")
+
+  const API_key = "3ba5d72b5215437dbb25e3ab5bc18d46"
+useEffect(()=>{
+axios.get(`https://api.rawg.io/api/games?key=${API_key}&dates=2019-09-01,2019-09-30&platforms=18,1,7`)
+.then((res)=>{
+  setGameName(res.data)
+  console.log(res.data);
+})
+},[])
   return (<><div className="body">
 
     <div className='latest'>
